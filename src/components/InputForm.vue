6a990c76feacdc0414a8bce92c50f48a764514a3
wowtk777 <wowtk777@example.com>
2020-07-28 11:25:35 +0300

    First form

---
+++ src/components/InputForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-form">
+        <form>
+            <label for="required-resources">Необходимое количество</label>
+            <input id="required-resources" v-model="requiredResources" type="text"/>
+            <br/>
+            <label for="ready-resources">Существующее количество</label>
+            <input id="ready-resources" v-model="readyResources" type="text"/>
+        </form>
+        <ResultGrid v-bind:required-resources="requiredResources" v-bind:ready-resources="readyResources" type="50"/>
+    </div>
+</template>
+
+<script>
+    import ResultGrid from "./ResultGrid";
+
+    export default {
+        name: 'InputForm',
+        data: function () {
+            return {
+                requiredResources: 0,
+                readyResources: 0,
+            }
+        },
+        components: {
+            ResultGrid
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/ResultGrid.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="result-grid">
+        <div v-for="pckg in packages" :key="pckg.size">
+            {{ pckg.size }}: {{ pckg.count }}
+        </div>
+    </div>
+</template>
+
+<script>
+    let preparePackages = function (type) {
+        return [1, 2, 4, 8, 12, 16, 20].map(x => x * type)
+    }
+    export default {
+        name: 'ResultGrid',
+        props: {
+            type: Number,
+            requiredResources: Number,
+            readyResources: Number,
+        },
+        computed: {
+            packages: function () {
+                return preparePackages(this.type).map(function (x) {
+                    return {
+                        size: x,
+                        count: 0
+                    }
+                })
+            }
+        }
+    }
+</script>
